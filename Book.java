f4e4bd381d59e74bdbbadeba01b211026a9d6bf4
ChazzCarter <70727845+ChazzCarter@users.noreply.github.com>
2020-09-30 13:49:10 -0400

    Added comment to test

--- Book.java
+++ Book.java
@@ -2,7 +2,7 @@
  * A class that maintains information on a book.
  * This might form part of a larger application such
  * as a library system, for instance.
- *
+ * This is working with a new workflow
  * @author (Chazz McGee)
  * @version (9/27/2020)
  */
